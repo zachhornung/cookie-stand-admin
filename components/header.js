40bdef81dccf0f905e5a21bb640671b1d3308baf
Zach Hornung <Zachhornung@comcast.net>
2021-07-07 20:48:03 -0700

    spent too much time looking at tailwind docs sorry

--- components/header.js
+++ components/header.js
@@ -1,7 +1,10 @@
+import Link from 'next/link'
+
 export default function Header(){
   return (
     <header className="p-6 bg-green-500">
       <h1 className="text-2xl font-semibold">Cookie Stand Admin</h1>
+      <Link href="./new_view"><a><h2>Check Out The Farm</h2></a></Link>
     </header>
   )
 }
